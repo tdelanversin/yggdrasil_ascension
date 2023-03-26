0aad17cdb585f40f00266b30a1a0d596e71e4073
Tilman de Lanversin <tilmand@student.ethz.ch>
2023-03-27 01:02:30 +0200

    ✨ add `ProjectileManager` and two interfaces `IProjectile` and `IShooter`

---
+++ src/ProjectileManger.cs
@@ -0,0 +1,61 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace YGR
+{
+
+    /// <summary>
+    /// Class <c>Factory_Projectiles</c> contains methods to create instances of <c>Y_Projectiles</c>.
+    /// </summary>
+    public static class ProjectileManager
+    {
+        public static Dictionary<string, Texture2D> projectile_textures;
+        private static List<IProjectile> _projectiles = new List<IProjectile>();
+        private static bool _initialized = false;
+        private static double _maxlifetime = 1500.0;
+
+        public static void Initialize(ContentManager content)
+        {
+            projectile_textures = new Dictionary<string, Texture2D>();
+            projectile_textures["default_projectile"] = content.Load<Texture2D>("projectile");
+
+            _initialized = true;
+        }
+
+        private static void check()
+        {
+            if (!_initialized) Logger.Error("Factory_Projectiles not initialized: call Factory_Projectiles.Initialize(ContentManager) somewhere!");
+        }
+
+        public static void AddProjectile(IProjectile projectile)
+        {
+            check();
+            _projectiles.Add(projectile);
+
+            Logger.Debug("Added projectile " + projectile.Name);
+        }
+
+        public static void Update(GameTime gameTime)
+        {
+            check();
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            foreach (IProjectile projectile in _projectiles)
+            {
+                projectile.Update(gameTime);
+            }
+
+            _projectiles.RemoveAll(projectile => projectile._timeCreated + _maxlifetime < gameTime.TotalGameTime.TotalMilliseconds);
+        }
+
+        public static void Draw(GameTime gameTime, Vector2 globalOffset, SpriteBatch spriteBatch)
+        {
+            check();
+            foreach (IProjectile projectile in _projectiles)
+            {
+                projectile.Draw(gameTime, globalOffset, spriteBatch);
+            }
+        }
+    }
+}
