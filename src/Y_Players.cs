4dd8e638ce1fad8d9bd10b748d7f770b34339ba0
arctize <patrick.wicki96@gmail.com>
2023-04-05 09:41:14 +0200

    use the keybindings class in sprites

--- src/Y_Players.cs
+++ src/Y_Players.cs
@@ -135,7 +135,6 @@ namespace YGR
         }
         public void Update(GameTime gameTime)
         {
-            //float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             int timeStepMS = gameTime.ElapsedGameTime.Milliseconds;
             GamePadState gpState = GamePad.GetState(_playerIndex);
             MouseState mouse = Mouse.GetState();
@@ -146,10 +145,10 @@ namespace YGR
             if (gpState.IsButtonDown(Buttons.LeftThumbstickDown)) input.Y -= gpState.ThumbSticks.Left.Y;
             if (gpState.IsButtonDown(Buttons.LeftThumbstickUp)) input.Y -= gpState.ThumbSticks.Left.Y;
 
-            if (Keyboard.IsPressed(Keys.D)) input.X += 1;
-            if (Keyboard.IsPressed(Keys.A)) input.X -= 1;
-            if (Keyboard.IsPressed(Keys.S)) input.Y += 1;
-            if (Keyboard.IsPressed(Keys.W)) input.Y -= 1;
+            if (Keyboard.IsPressed(Keybinds.P1Right)) input.X += 1;
+            if (Keyboard.IsPressed(Keybinds.P1Left)) input.X -= 1;
+            if (Keyboard.IsPressed(Keybinds.P1Down)) input.Y += 1;
+            if (Keyboard.IsPressed(Keybinds.P1Up)) input.Y -= 1;
 
             if (input.LengthSquared() > 1)
             {
@@ -195,7 +194,7 @@ namespace YGR
                 _dashCooldownTimer += timeStepMS;
             }
 
-            if (!_isDashing && Keyboard.IsPressed(Keys.Space) && _dashCooldownTimer >= _dashCooldown)
+            if (!_isDashing && Keyboard.IsPressed(Keybinds.P1Dash) && _dashCooldownTimer >= _dashCooldown)
             {
                 _isDashing = true;
                 _dashTimer = 0;
