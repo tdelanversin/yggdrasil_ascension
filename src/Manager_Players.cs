1e5a0b75397e05fb1d9ec3dafe4112040cc224b4
arctize <patrick.wicki96@gmail.com>
2023-04-19 13:54:34 +0200

    player: add new Manager_Players and SimplePlayer

---
+++ src/Manager_Players.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace YGR
+{
+    public static class Manager_Players
+    {
+
+        // Player list
+        public static IList<IVictim> Players { get; private set; }
+
+        // Sprites
+        public static Texture2D SpriteNinja { get; private set; }
+        public static Texture2D SpriteBasic { get; private set; }
+        public static Texture2D SpriteGhost { get; private set; }
+        public static IList<Texture2D> SpriteAimIndicator { get; private set; }
+
+        public static void LoadContent(ContentManager contentManager)
+        {
+            SpriteNinja = contentManager.Load<Texture2D>("charaset");
+            SpriteBasic = contentManager.Load<Texture2D>("tester_60");
+            SpriteGhost = contentManager.Load<Texture2D>("ghost");
+            SpriteAimIndicator = new List<Texture2D> {
+                contentManager.Load<Texture2D>("target_indicator_red"),
+                contentManager.Load<Texture2D>("target_indicator_blue"),
+                contentManager.Load<Texture2D>("target_indicator_green"),
+                contentManager.Load<Texture2D>("target_indicator_yellow"),
+            };
+        }
+
+        public static void Initialize()
+        {
+            Players = new List<IVictim>();
+        }
+
+        public static void ClearPlayers()
+        {
+            Players.Clear();
+        }
+
+        public static void AddPlayer_SimplePlayer(
+            PlayerIndex playerIndex,
+            Vector2 position,
+            Y_Level level,
+            ControlLayout controlLayout = ControlLayout.ControllerOnly)
+        {
+            Players.Add(new SimplePlayer(
+                playerIndex,
+                position,
+                level,
+                new Y_WideGun(),
+                ControlLayout.ControllerOnly,
+                scale: 1.0f
+            ));
+        }
+
+        public static void AddPlayer_Ninja(
+            PlayerIndex playerIndex,
+            Vector2 position,
+            Y_Level level,
+            ControlLayout controlLayout = ControlLayout.ControllerOnly
+        )
+        {
+            // TODO: clean this up after improving player -> ninja subclassing
+            Players.Add(new Ninja(
+                    new X_CollisionModel_Victim(1.0f /* mass */, 0.0f /* elastic impact */),
+                    playerIndex,
+                    SpriteNinja,
+                    SpriteGhost,
+                    SpriteAimIndicator[(int)playerIndex],
+                    0.02f,
+                    position,
+                    level,
+                    new Y_StarterGun(),
+                    (int)controlLayout,
+                    1.0f
+                ));
+        }
+
+        internal static void Update(GameTime gameTime)
+        {
+            foreach (var player in Players)
+            {
+                player.Update(gameTime);
+            }
+        }
+
+        internal static void Draw(GameTime gameTime, Vector2 globalOffset, SpriteBatch spriteBatch)
+        {
+            foreach (var player in Players)
+            {
+                player.Draw(gameTime, globalOffset, spriteBatch);
+            }
+        }
+
+        internal static void DrawOutline(GameTime gameTime, Vector2 globalOffset, SpriteBatch spriteBatch)
+        {
+            foreach (var player in Players)
+            {
+                player.DrawOutline(gameTime, globalOffset, spriteBatch);
+            }
+        }
+    }
+}
--- src/Y_Player.cs
+++ src/Y_Player.cs
@@ -6,6 +6,295 @@ using System.Collections.Generic;
 
 namespace YGR
 {
+    public enum ControlLayout
+    {
+        ControllerOnly = 0,
+        KeyboardWASD,
+        KeyboardArrows,
+    }
+
+    public class SimplePlayer : IVictim
+    {
+        // IGameElement fields
+        public float Scale { get; private set; }
+        public Rectangle Rect { get { return _rect; } set { _rect = value; } }
+
+        // IVictim fields
+        public int LifePoints { get; set; }
+        public bool HitInLastLoop { get; set; }
+        public X_CollisionModel_Victim Collision { get; }
+        public Vector2 Velocity { get; set; }
+        public Y_Level Level { get; set; }
+        public IWalkable Room { get; set; }
+
+        // private fields
+        Texture2D _spritePlayer;
+        Texture2D _spriteGhost;
+        Texture2D _spriteAimIndicator;
+        Rectangle _spriteDimensions;
+        Dictionary<string, int[]> _animations;
+        int _animationIndex;
+        IShooter _gun;
+        PlayerIndex _playerIndex;
+        X_ConnectorSide _lastSide;
+        bool _isAiming;
+        Vector2 _aimDirection;
+        private Vector2 _acceleration;
+        private Vector2 _deceleration;
+        private Vector2 _maxVelocity;
+        private float _mass;
+        private float _cr;
+        private Rectangle _rect;
+        private Vector2 _position;
+        private ControlLayout _controlLayout;
+
+        public SimplePlayer(
+            PlayerIndex playerIndex,
+            Vector2 initialPosition,
+            Y_Level level,
+            IShooter gun,
+            ControlLayout controlLayout = ControlLayout.ControllerOnly,
+            float scale = 1.0f
+            )
+        {
+            // Use all constructor arguments
+            Level = level;
+            _playerIndex = playerIndex;
+            _position = initialPosition;
+            _gun = gun;
+            _controlLayout = controlLayout;
+
+            // Set all the sprites
+            _spritePlayer = Manager_Players.SpriteBasic;
+            _spriteGhost = Manager_Players.SpriteGhost;
+            _spriteAimIndicator = Manager_Players.SpriteAimIndicator[(int)playerIndex];
+
+            _spriteDimensions = new Rectangle(0, 0, 42, 60);
+            _animationIndex = 0;
+            _animations = new Dictionary<string, int[]> {
+                { "stand", new int[] { 0, 1, 8, 9 } },
+                { "walk_left", new int[] { 2, 3, 4 } },
+                { "walk_right", new int[] { 5, 6, 7 } }};
+
+            Velocity = Vector2.Zero;
+            _acceleration = Vector2.One * 0.004f;
+            _deceleration = Vector2.One * 0.004f;
+            _maxVelocity = Vector2.One * 0.4f;
+
+            _mass = 1.0f;
+            _cr = 0.0f; // elastic impact
+            Collision = new X_CollisionModel_Victim(_mass, _cr);
+
+            _isAiming = false;
+            _aimDirection = Vector2.Zero;
+
+            LifePoints = 30;
+            HitInLastLoop = false;
+
+            _rect = new Rectangle(
+                (int)_position.X - (int)(scale * _spriteDimensions.Width / 2),
+                (int)_position.Y - (int)(scale * _spriteDimensions.Height / 2),
+                (int)(scale * _spriteDimensions.Width), (int)(scale * _spriteDimensions.Height)
+            );
+
+            Scale = (float)Rect.Width / (float)_spriteDimensions.Width;
+            Level.Victims.Add(this);
+            Room = Level.GetRoom(this, Room);
+        }
+
+        public X_LevelElements WhatAreYou()
+        {
+            if (LifePoints < 1)
+            {
+                return X_LevelElements.Ghost;
+            }
+            else
+            {
+                return X_LevelElements.Victim;
+            }
+        }
+
+        /// <summary>
+        /// Regular Monogame Update method
+        /// </summary>
+        /// <param name="gameTime">Monogame GameTime</param>
+        public void Update(GameTime gameTime)
+        {
+            Vector2 input = Vector2.Zero;
+            GamePadState gpState = GamePad.GetState(_playerIndex);
+
+            if (HitInLastLoop)
+            {
+                HitInLastLoop = false;
+            }
+
+            if (LifePoints < 1)
+            {
+                return; // he dead
+            }
+
+            // Gamepad control
+            if (gpState.IsConnected)
+            {
+                if (gpState.IsButtonDown(Buttons.LeftThumbstickRight)) input.X += gpState.ThumbSticks.Left.X;
+                if (gpState.IsButtonDown(Buttons.LeftThumbstickLeft)) input.X += gpState.ThumbSticks.Left.X;
+                if (gpState.IsButtonDown(Buttons.LeftThumbstickDown)) input.Y -= gpState.ThumbSticks.Left.Y;
+                if (gpState.IsButtonDown(Buttons.LeftThumbstickUp)) input.Y -= gpState.ThumbSticks.Left.Y;
+
+                if (
+                    gpState.IsButtonDown(Buttons.RightThumbstickRight) ||
+                    gpState.IsButtonDown(Buttons.RightThumbstickLeft) ||
+                    gpState.IsButtonDown(Buttons.RightThumbstickDown) ||
+                    gpState.IsButtonDown(Buttons.RightThumbstickUp)
+                )
+                {
+                    Vector2 newAimDirection = Vector2.One;
+                    newAimDirection.X *= gpState.ThumbSticks.Right.X;
+                    newAimDirection.Y *= -gpState.ThumbSticks.Right.Y;
+                    newAimDirection.Normalize();
+                    _aimDirection = newAimDirection;
+                    _isAiming = true;
+                }
+                else
+                {
+                    _isAiming = false;
+                }
+                if (gpState.IsButtonDown(Buttons.RightShoulder) || gpState.IsButtonDown(Buttons.RightTrigger))
+                {
+                    _isAiming = true; // Show the aim indicator when firing
+                    _gun.Shoot(gameTime, Rect.Center.ToVector2(), _aimDirection, Level, this);
+                }
+            }
+
+            // Mouse & Keyboard control
+            if (_controlLayout > 0)
+            {
+                if (_controlLayout == ControlLayout.KeyboardWASD)
+                {
+                    if (Input.IsKeyDown(Keybinds.P1Right)) input.X += 1;
+                    if (Input.IsKeyDown(Keybinds.P1Left)) input.X -= 1;
+                    if (Input.IsKeyDown(Keybinds.P1Down)) input.Y += 1;
+                    if (Input.IsKeyDown(Keybinds.P1Up)) input.Y -= 1;
+                }
+                else if (_controlLayout == ControlLayout.KeyboardArrows)
+                {
+                    if (Input.IsKeyDown(Keybinds.P2Right)) input.X += 1;
+                    if (Input.IsKeyDown(Keybinds.P2Left)) input.X -= 1;
+                    if (Input.IsKeyDown(Keybinds.P2Down)) input.Y += 1;
+                    if (Input.IsKeyDown(Keybinds.P2Up)) input.Y -= 1;
+                }
+                MouseState mouse = Mouse.GetState();
+                Vector2 playerCenter = Rect.Center.ToVector2();
+                if (Input.HasMouseMoved() && !_isAiming) // Skip if controller is already aiming
+                {
+                    Vector2 mouseInGamePosition = mouse.Position.ToVector2() / Camera.Zoom + Camera.VisibleArea.Location.ToVector2();
+                    Vector2 newAimDirection = mouseInGamePosition - playerCenter;
+                    newAimDirection.Normalize();
+                    _aimDirection = newAimDirection;
+                }
+                if (mouse.LeftButton == ButtonState.Pressed)
+                {
+                    _gun.Shoot(gameTime, playerCenter, _aimDirection, Level, this);
+                }
+            }
+
+            _gun.Update(gameTime);
+
+            bool controls = false;
+            if (input != Vector2.Zero) controls = true;
+            if (input.LengthSquared() > 1)
+            {
+                input.Normalize();
+            }
+
+
+            /* ##########################################################################
+             * Speed and velocity handling based on control input
+             *  => must happen before collision handling <=
+             * ########################################################################## */
+            int timeStepMS = gameTime.ElapsedGameTime.Milliseconds;
+            //Logger.Info(Velocity.ToString() + "    " + MaxVelocity.ToString());
+            if (controls)
+            {
+                Velocity += input * _acceleration * timeStepMS;
+            }
+            else
+            {
+                Velocity = new Vector2(
+                    Math.Sign(Velocity.X) * Math.Max(0.0f, Math.Abs(Velocity.X) - _deceleration.X * timeStepMS),
+                    Math.Sign(Velocity.Y) * Math.Max(0.0f, Math.Abs(Velocity.Y) - _deceleration.Y * timeStepMS));
+            }
+            Velocity = Vector2.Clamp(Velocity, -_maxVelocity, _maxVelocity);
+
+            /* ##########################################################################
+             * Collision with everything handling (takes care of location update as well)
+             * ########################################################################## */
+            IList<Vector2> contactNormal;
+            IList<Point> contactPoint;
+            IList<IGameElement> who;
+            Vector2 newVelocity = Velocity;
+            if (Collision.Intersect(this, timeStepMS, out newVelocity, out contactPoint, out contactNormal, out who))
+            {
+                //Logger.Info("Collided with something");
+                Velocity = newVelocity;
+            }
+            //Rectangle rect = me.Rect;
+            //rect.Location += (me.Velocity * timeStepMS).ToPoint();
+            //me.Rect = rect;
+
+            _position += newVelocity * timeStepMS;
+            _rect.Location = _position.ToPoint();
+            /* ########################################################################## */
+        }
+
+        /// <summary>
+        /// Regular Draw method for all drawable objects
+        /// </summary>
+        /// <param name="gameTime">Monogame GameTime</param>
+        /// <param name="globalOffset">If you don't know what, put Vector2.Zero</param>
+        /// <param name="spriteBatch">Active Monogame SpriteBatch</param>
+        public void Draw(GameTime gameTime, Vector2 globalOffset, SpriteBatch spriteBatch)
+        {
+            Color color = Color.White;
+            if (LifePoints < 1)
+            {
+                // Render ghosty 👻
+                spriteBatch.Draw(
+                    _spriteGhost,
+                    new Rectangle(_rect.X, _rect.Y, _rect.Width, _rect.Height),
+                    new Rectangle(0, 0, 180, 180), Color.White);
+                return;
+            }
+
+            spriteBatch.Draw(
+                _spritePlayer, _rect.Location.ToVector2(),
+                new Rectangle(_animationIndex * _spriteDimensions.Width, 0, _spriteDimensions.Width, _spriteDimensions.Height),
+                Color.White, 0, Vector2.Zero, Scale, SpriteEffects.None, 0);
+            spriteBatch.DrawString(Fonts.Normal, LifePoints.ToString(), new Vector2(_rect.Location.X + 30, _rect.Location.Y - 10), Color.Wheat);
+
+            // Draw a targeting indicator if the player is actively aiming or using mouse controls
+            if (_isAiming || _controlLayout > 0)
+            {
+                var angle = Math.Atan2(_aimDirection.Y, _aimDirection.X) + Math.PI / 2;
+                spriteBatch.Draw(
+                    _spriteAimIndicator, _rect.Location.ToVector2() + _spriteDimensions.Center.ToVector2() + _aimDirection * _spritePlayer.Height,
+                    null,
+                    Color.White, (float)angle, new Vector2(_spriteAimIndicator.Width / 2, 0), 0.1f, SpriteEffects.None, 0);
+            }
+        }
+
+        /// <summary>
+        /// Regular DrawOutline method for debugging
+        /// </summary>
+        /// <param name="gameTime">Monogame GameTime object</param>
+        /// <param name="globalOffset">If it's not clear, then Vector2.Zero</param>
+        /// <param name="spriteBatch">Mogogame SpriteBatch</param>
+        public void DrawOutline(GameTime gameTime, Vector2 globalOffset, SpriteBatch spriteBatch)
+        {
+            Factory_Debug.DrawRectangle(_rect.X, _rect.Y, _rect.Width, _rect.Height, 1, Color.OrangeRed, spriteBatch);
+            Collision.DrawOutline(gameTime, globalOffset, spriteBatch);
+        }
+    }
     public class Ninja : IVictim // Y_Sprite
     {
         private bool _isDashing;
@@ -137,9 +426,9 @@ namespace YGR
             Rectangle rr = directionSourceRectangles["down"][0];
             _position = position;
             _rect = new Rectangle(
-                (int)position.X - (int)(scale*rr.Width / 2),
-                (int)position.Y - (int)(scale*rr.Height / 2),
-                (int)(scale*rr.Width), (int)(scale*rr.Height)
+                (int)position.X - (int)(scale * rr.Width / 2),
+                (int)position.Y - (int)(scale * rr.Height / 2),
+                (int)(scale * rr.Width), (int)(scale * rr.Height)
             );
 
             // This tells the animation to start on the left-side sprite.
