b281cf800a2aa6e81758516799a7e8a0ece46437
arctize <patrick.wicki96@gmail.com>
2023-04-29 10:58:56 +0200

    camera: use sigmoid for even more fluid animations

--- src/Y_Camera.cs
+++ src/Y_Camera.cs
@@ -177,6 +177,48 @@ namespace YGR
             }
         }
 
+        private static double SigmoidCentered(double t, double a)
+        {
+            return (1d / (1 + Math.Exp(-a * t)) - 0.5d);
+        }
+
+        private static double EaseInOut(double t, double k)
+        {
+            return (0.5d / SigmoidCentered(1, k)) * SigmoidCentered(2 * t - 1, k) + 0.5d;
+        }
+
+        private static void UpdateAnimation(GameTime gameTime)
+        {
+
+            if (_animationTimer < _animationDuration)
+            {
+
+                // var animationFraction = _animationTimer / _animationDuration; // linear
+
+                // Hand-made Slow-in, slow-out function, using sin(): (1+sin(pi*x/500-pi/2))/2
+                // https://www.wolframalpha.com/input?i=plot+%281%2Bsin%28pi*x%2F1-pi%2F2%29%29%2F2+from+-0.2+to+1.2
+                // float animationFraction = (float)(1d + Math.Sin(Math.PI * _animationTimer / _animationDuration - Math.PI / 2d)) / 2f;
+
+                // EaseInOut using Sigmoid and a high steepnes factor
+                // https://www.wolframalpha.com/input?i=plot+1+%2F+%281+%2B+exp%28-12%28x+-+1%2F2%29%29%29+from+-0.2+to+1.2
+                // float animationFraction = (float)(1f / (1 + Math.Exp(-16 * (_animationTimer / _animationDuration - 0.5d))));
+
+                // Even better version that works for low factors of k too
+                // https://www.wolframalpha.com/input?i=plot+%28%280.5+%2F+%281%2F%281%2Bexp%28-k%29%29-0.5%29%29*%281%2F%281%2Bexp%28-k*%282x-1%29%29%29-0.5%29%2B0.5%29+x+from+-0.2+to+1.2%2C+k+%3D+8
+                float animationFraction = (float)EaseInOut(t: _animationTimer / _animationDuration, k: 6d);
+
+                _transitionalPosition = (Position * animationFraction) + _previousPosition * (1f - animationFraction);
+                _transitionalZoom = (Zoom * animationFraction) + _previousZoom * (1f - animationFraction);
+
+                _animationTimer += gameTime.ElapsedGameTime.Milliseconds;
+            }
+            else
+            {
+                _transitionalPosition = Position;
+                _transitionalZoom = Zoom;
+            }
+        }
+
         public static void Update(Viewport bounds, GameTime gameTime)
         {
             if (Input.IsKeyTriggered(Keys.F10))
@@ -200,22 +242,7 @@ namespace YGR
                     break;
             }
             Bounds = bounds.Bounds;
-
-            if (_animationTimer < _animationDuration)
-            {
-                // var animationFraction = _animationTimer / _animationDuration; // linear
-                // Hand-made Slow-in, slow-out function, using sin(): (1+sin(pi*x/500-pi/2))/2
-                float animationFraction = (float)(1d + Math.Sin(Math.PI * _animationTimer / _animationDuration - Math.PI / 2d)) / 2f;
-                _transitionalPosition = (Position * animationFraction) + _previousPosition * (1f - animationFraction);
-                _transitionalZoom = (Zoom * animationFraction) + _previousZoom * (1f - animationFraction);
-                _animationTimer += gameTime.ElapsedGameTime.Milliseconds;
-            }
-            else
-            {
-                _transitionalPosition = Position;
-                _transitionalZoom = Zoom;
-            }
-
+            UpdateAnimation(gameTime);
             UpdateMatrix();
         }
 
