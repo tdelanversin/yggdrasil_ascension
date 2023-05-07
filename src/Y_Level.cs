91241d943223770d09d9090e4e9455eef858b216
Dude <blub1.blub2@gmail.com>
2023-05-07 22:14:50 +0200

    adapt positions of a bunch of rooms and make the background a tiny bit bigger to have more space

--- src/Y_Level.cs
+++ src/Y_Level.cs
@@ -143,8 +143,8 @@ namespace YGR
             Rooms = new Dictionary<int, IWalkable>();
 
             _background = content.Load<Texture2D>("SpritesOther/Level_Background");
-            int width = (int)(_background.Width * 3.5f);
-            int height = (int)(_background.Height * 3.5f);
+            int width = (int)(_background.Width * 3.8f);
+            int height = (int)(_background.Height * 3.8f);
             Point startLocation = new Point((int)(width / 1.96f), (int)(height / 1.137f));
             _backgroundRect = new Rectangle(-startLocation.X, -startLocation.Y, width, height);
 
@@ -186,8 +186,8 @@ namespace YGR
 
             var random = new Random();
             // randomly select one level tree
-            var key = _data.Level.Keys.ToArray()[random.Next(0, _data.Level.Keys.Count)];
-            //var key = _data.Level.Keys.ToArray()[0];
+            //var key = _data.Level.Keys.ToArray()[random.Next(0, _data.Level.Keys.Count)];
+            var key = _data.Level.Keys.ToArray()[3];
             var tree = _data.Level[key];
 
             var watch = new Stopwatch();
