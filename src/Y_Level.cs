539c6cd71ff6defb52ba3b52db9ce946453db8c6
Dude <blub1.blub2@gmail.com>
2023-05-07 17:56:46 +0200

    Add like 10 level variations

--- src/Y_Level.cs
+++ src/Y_Level.cs
@@ -191,8 +191,7 @@ namespace YGR
 
             var random = new Random();
             // randomly select one level tree
-            //var key = _data.Level.Keys.ToArray()[random.Next(0, _data.Level.Keys.Count)];
-            var key = _data.Level.Keys.ToArray()[_data.Level.Keys.ToArray().Length-1];
+            var key = _data.Level.Keys.ToArray()[random.Next(0, _data.Level.Keys.Count)];
             var tree = _data.Level[key];
 
             var watch = new Stopwatch();
