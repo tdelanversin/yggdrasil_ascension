9fe091b7697c2c41063e8c150094bc54c1702fb4
arctize <patrick.wicki96@gmail.com>
2023-05-09 17:57:09 +0200

    settings: implement json settings store and load

--- src/Y_Settings.cs
+++ src/Y_Settings.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
 
 namespace YGR
 {
+
+    public sealed class SettingsObject
+    {
+        public DateTime Datetime;
+        public int Version;
+        public bool Fullscreen;
+        public bool DynamicShades;
+        public bool Outlines;
+        public bool DrawFPS;
+        public bool Sound;
+        public bool Music;
+
+        public SettingsObject()
+        {
+            Datetime = DateTime.Now;
+        }
+    }
 
     public static class Settings
     {
@@ -20,6 +40,8 @@ namespace YGR
         public static bool Sound;
         public static bool Music;
 
+        public static string SettingsPath;
+
         public static void Initialize(A_Yggdrasil game)
         {
             Game = game;
@@ -27,36 +49,90 @@ namespace YGR
             Window = game.Window;
 
             // Defaults
+            Fullscreen = true;
             DrawFPS = true; // Always on for now, shouldn't really bother anyone
+            DynamicShades = false; // Off by default, not because they are slow (they are in fact very quick) but because Monogame sucks so much!!!
+            Outlines = false;
             Sound = true;
             Music = true;
 #if DEBUG
             Fullscreen = false;
-            DynamicShades = false;
-            Outlines = false;
-            ToggleMusic();
-#else
-            Fullscreen = true;
-            DynamicShades = false; // Off by default, not because they are slow (they are in fact very quick) but because Monogame sucks so much!!!
             Outlines = false;
 #endif
+
+            var LocalAppDataDir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+            var GameDataDir = System.IO.Path.Combine(LocalAppDataDir, "YggdrasilAscension");
+            var DirInfo = System.IO.Directory.CreateDirectory(GameDataDir);
+            SettingsPath = Path.Combine(GameDataDir, "settings.json");
+
+            if (File.Exists(SettingsPath))
+            {
+                SettingsObject settings = Util.LoadJSON<SettingsObject>(SettingsPath)[0];
+                LoadSettings(settings);
+            }
+            else
+            {
+                SaveSettings();
+            }
+
+            ApplySoundSettings();
+        }
+
+        private static void LoadSettings(SettingsObject so)
+        {
+            Fullscreen = so.Fullscreen;
+            DynamicShades = so.DynamicShades;
+            Outlines = so.Outlines;
+            DrawFPS = so.DrawFPS;
+            Sound = so.Sound;
+            Music = so.Music;
+        }
+
+        private static void SaveSettings()
+        {
+            SettingsObject settings = new SettingsObject();
+            settings.Fullscreen = Fullscreen;
+            settings.DynamicShades = DynamicShades;
+            settings.Outlines = Outlines;
+            settings.DrawFPS = DrawFPS;
+            settings.Sound = Sound;
+            settings.Music = Music;
+
+            var settingsData = new List<SettingsObject> { settings };
+            Util.WriteJSON(SettingsPath, settingsData);
+        }
+
+        internal static void ApplySoundSettings()
+        {
+            if (!Sound)
+            {
+                ToggleSoundEffects();
+            }
+
+            if (!Music)
+            {
+                ToggleSoundEffects();
+            }
         }
 
         internal static bool ToggleShades()
         {
             DynamicShades = !DynamicShades;
+            SaveSettings();
             return DynamicShades;
         }
 
         internal static bool ToggleOutlines()
         {
             Outlines = !Outlines;
+            SaveSettings();
             return Outlines;
         }
 
         internal static bool ToggleDrawFPS()
         {
             DrawFPS = !DrawFPS;
+            SaveSettings();
             return DrawFPS;
         }
 
@@ -92,6 +168,7 @@ namespace YGR
                 ApplyScreenConfiguration();
                 Logger.Debug("Turning fullscreen ON. Resolution: " + Gdm.PreferredBackBufferWidth.ToString() + "x" + Gdm.PreferredBackBufferHeight.ToString());
             }
+            SaveSettings();
             return Fullscreen;
         }
 
@@ -106,10 +183,12 @@ namespace YGR
             {
                 SoundEffect.MasterVolume = 1f;
             }
+            SaveSettings();
             return Sound;
         }
 
-        public static bool ToggleMusic(){
+        public static bool ToggleMusic()
+        {
             Music = !Music;
             if (!Music)
             {
@@ -119,6 +198,7 @@ namespace YGR
             {
                 MediaPlayer.IsMuted = false;
             }
+            SaveSettings();
             return Music;
         }
     }
