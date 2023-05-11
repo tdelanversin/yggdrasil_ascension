343af00c6988b5c2749dc2265f7fb1c7151a49d2
arctize <patrick.wicki96@gmail.com>
2023-05-11 18:43:26 +0200

    pickups: add colors to pickups

--- src/Y_PickUp.cs
+++ src/Y_PickUp.cs
@@ -40,6 +40,8 @@ namespace YGR
 
         public Y_PowerUps Type { get; }
 
+        public Color Color;
+
         public Func<IPlayer, PickUp, bool> Action { get; }
         public int ElementLevel { get { return 1; } set { } }
 
@@ -76,6 +78,7 @@ namespace YGR
         {
             LocalScale = scale * Y_Level.GlobalScale;
             Type = type;
+            Color = Color.White;
 
             Action = action;
             Active = true;
@@ -95,6 +98,8 @@ namespace YGR
         {
             LocalScale = scale * Y_Level.GlobalScale;
             Type = type;
+            Color = Color.White;
+
             int heightNew = (int)(height * scale);
             int widthNew = (int)((heightNew * texture.Width / texture.Height));
 
@@ -105,6 +110,12 @@ namespace YGR
             _spriteDrawScale = LocalScale * Util.GetSpriteScale(Rect, texture.Bounds.Size.ToVector2());
 
             _lastOwner = lastOwner;
+        }
+
+        public PickUp(Y_PowerUps type, Point location, int width, int height, float scale, Texture2D texture, IVictim lastOwner, Color color, Func<IPlayer, PickUp, bool> action)
+        : this(type, location, width, height, scale, texture, lastOwner, action)
+        {
+            Color = color;
         }
 
         public static PickUp Factory(Y_PowerUps type, Point location, int width, int height, float scale, IVictim lastOwner = null)
@@ -202,7 +213,7 @@ namespace YGR
                             return switchGun(new Gun_Funky(player), player, self);
                         });
                 case Y_PowerUps.WeaponPinkHammer:
-                    return new PickUp(type, location, width, height, _gunScale, Manager_Sprites.Weapon_Hammer, lastOwner,
+                    return new PickUp(type, location, width, height, _gunScale, Manager_Sprites.Weapon_Hammer, lastOwner, Color.LightPink,
                         (player, self) =>
                         {
                             if (player.Gun.GetType() == typeof(Weapon_PinkHammer))
@@ -271,7 +282,7 @@ namespace YGR
                         texture: _texture,
                         destinationRectangle: new Rectangle(Rect.X + (Rect.Width - spinWidth) / 2, Rect.Y, spinWidth, Rect.Height),
                         sourceRectangle: null,
-                        color: Color.White,
+                        color: Color,
                         rotation: 0,
                         origin: Vector2.Zero,
                         effects: spin >= 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally,
