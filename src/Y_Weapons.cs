ba0ec5a93eec699be6537d92e3e9756ec1738df1
arctize <patrick.wicki96@gmail.com>
2023-05-11 18:26:39 +0200

    weapons: add the god mode warhammer

---
+++ src/Y_Weapons.cs
@@ -0,0 +1,28 @@
+using Microsoft.Xna.Framework;
+#nullable enable
+
+namespace YGR
+{
+    public class Weapon_PinkHammer : Gun_Basic
+    {
+        public Weapon_PinkHammer(IVictim owner) : base(owner)
+        {
+            ShotDelay = 1000;
+            Name = "Pink Hammer";
+            Sprite = Manager_Sprites.Weapon_Hammer;
+        }
+
+        public override bool Shoot(GameTime gameTime, Vector2 origin, Vector2 direction, Y_Level level, IGameElement who)
+        {
+            if (NextShotCooldown > 0.0f)
+                return false;
+
+            Manager_Sound.Sound_Fireball.Play(0.2f, 0, 0);
+
+            NextShotCooldown = ShotDelay;
+
+            Manager_Projectile.AddProjectile_PinkHammer(origin, direction, level, who);
+            return true;
+        }
+    }
+}
