60a6a17470d092cf8c7e7759b057a4f2623b4133
Dude147 <dumeni.manatschal@gmail.com>
2023-05-11 23:40:39 +0200

    try some parallel stuff to check collision etc

--- src/Manager_Projectile.cs
+++ src/Manager_Projectile.cs
@@ -3,7 +3,10 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace YGR
 {
@@ -166,11 +169,24 @@ namespace YGR
         public static void Update(GameTime gameTime)
         {
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Parallel.ForEach(_projectiles, projectile =>
+            {
+                projectile.UpdateCollisionAndVelocity(gameTime);
+            });
+
             foreach (IProjectile projectile in _projectiles)
             {
                 projectile.Update(gameTime);
             }
-            _projectiles.RemoveAll(projectile => (projectile.Age > projectile.MaxAge) || projectile.DeleteNext);
+
+            //_projectiles.RemoveAll(projectile => (projectile.Age > projectile.MaxAge) || projectile.DeleteNext);
+
+            //var newList = _projectiles.AsParallel()
+            //   .Where(projectile => !((projectile.Age > projectile.MaxAge) || projectile.DeleteNext))
+            //.ToList();
+            //_projectiles = newList;
+
+            _projectiles = _projectiles.Where(projectile => !((projectile.Age > projectile.MaxAge) || projectile.DeleteNext)).ToList();
         }
 
         public static void Draw(GameTime gameTime, Vector2 globalOffset, SpriteBatch spriteBatch)
