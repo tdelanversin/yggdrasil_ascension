2ef338719a6a6fe94c150f37615b41acc291c83d
Dude <blub1.blub2@gmail.com>
2023-05-14 20:22:14 +0200

    change keybinds for shield to 'LeftAlt' because my mouse right-click sucks

--- src/Y_Keybinds.cs
+++ src/Y_Keybinds.cs
@@ -18,6 +18,7 @@ namespace YGR
         public static Buttons GamePadShoot = Buttons.RightTrigger;
         public static Buttons GamePadAction = Buttons.LeftTrigger;
         public static Buttons GamePadAbility = Buttons.RightShoulder;
+        public static Keys KeyboardAbility = Keys.LeftAlt;
 
         public static Keys Enter = Keys.Enter;
 
