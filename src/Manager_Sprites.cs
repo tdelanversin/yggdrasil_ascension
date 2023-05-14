f891f86c2a6874d042684003f79e10bc5e9b4ccb
colonnad <colonnad@student.ethz.ch>
2023-05-14 14:29:28 +0200

    added images and show them when needed, still working on animations

--- src/Manager_Sprites.cs
+++ src/Manager_Sprites.cs
@@ -75,6 +75,8 @@ namespace YGR
         public static Texture2D LevelUp_DarkFlash_NoShade { get; private set; }
         public static Texture2D LevelUp_LightFlash_NoShade { get; private set; }
         public static Texture2D LevelUp_Bullet_NoShade { get; private set; }
+        public static Texture2D defeat_image { get; private set; }
+        public  static Texture2D victory_image { get; private set; }    
 
         public static void LoadContent(ContentManager contentManager)
         {
@@ -136,6 +138,9 @@ namespace YGR
             LevelUp_DarkFlash_NoShade = contentManager.Load<Texture2D>("SpritesOther/thunder_up_sheet_2_no_shade");
             LevelUp_LightFlash_NoShade = contentManager.Load<Texture2D>("SpritesOther/tunder_up_sheet_no_shade");
             LevelUp_Bullet_NoShade = contentManager.Load<Texture2D>("SpritesOther/bullet_sheet_no_shade");
+
+            defeat_image = contentManager.Load<Texture2D>("Images/defeat");
+            victory_image = contentManager.Load<Texture2D>("Images/victory");
         }
 
         public static AnimatedSprite NewAnimatedSprite_LevelUp_DarkFlash_NoShade()
