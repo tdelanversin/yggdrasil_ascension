ec246e79c1185fa2b552d515f01a5a34dc677271
Tilman de Lanversin <tilmand@student.ethz.ch>
2023-05-27 16:29:40 +0200

    🎨 tweak to lerp

--- src/Y_Projectile_Basic.cs
+++ src/Y_Projectile_Basic.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System;
 #nullable enable
 
 namespace YGR
@@ -121,7 +122,7 @@ namespace YGR
             if (obj is IEnemy)
             {
                 Vector2 pos = new Vector2(_rect.Location.X + _rect.Width / 2, _rect.Location.Y + _rect.Height / 2);
-                pos = Vector2.Lerp(pos, obj.Rect.Center.ToVector2(), Util.random.NextSingle()); // move towards center of enemy (looks better)
+                pos = Vector2.Lerp(pos, obj.Rect.Center.ToVector2(), (float) Math.Sqrt(Util.random.NextDouble() * .7)); // move towards center of enemy (looks better)
                 Vector2 nor = contactNormal;
                 Manager_Particles.MakeSlimeImpactParticle((IEnemy)obj, pos, nor);
             }
